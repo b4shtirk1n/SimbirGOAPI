6b27b1eb77cefa6cce339a0bdf59bea63d7a1fb1
b4shtirk1n <wowan4512@gmail.com>
2023-10-16 10:25:13 +0500

    add dependencies

--- SimbirGOAPI/obj/Debug/net7.0/SimbirGOAPI.AssemblyInfo.cs
+++ SimbirGOAPI/obj/Debug/net7.0/SimbirGOAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimbirGOAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87d8293a26860f5e5a8ae8145f36095bc429b919")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimbirGOAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimbirGOAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
