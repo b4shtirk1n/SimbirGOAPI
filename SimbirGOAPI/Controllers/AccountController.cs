87d8293a26860f5e5a8ae8145f36095bc429b919
b4shtirk1n <wowan4512@gmail.com>
2023-10-16 09:59:35 +0500

    first commit

---
+++ SimbirGOAPI/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimbirGOAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            this.logger = logger;
+        }
+    }
+}
