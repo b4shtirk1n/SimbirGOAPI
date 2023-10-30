8a8902800a87adf2c62a8320675153fc1cf86629
b4shtirk1n <wowan4512@gmail.com>
2023-10-30 16:02:18 +0500

    edit gitignore

--- SimbirGOAPI/obj/Release/net7.0/SimbirGOAPI.AssemblyInfo.cs
+++ SimbirGOAPI/obj/Release/net7.0/SimbirGOAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimbirGOAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eaee8d605d5f2f3b20125e18428c555c7115ce5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimbirGOAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimbirGOAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
